0f7ff1b10d7b51a61f612141edd50041de709aa6
quochn81 <quochn81@gmail.com>
2019-09-11 13:34:35 +0700

    Update problem4.js

--- problem4.js
+++ problem4.js
@@ -2,7 +2,6 @@ check_room = (arr1, arr2, n) => {
 	let total_room = n;
 	let arrivals = arr2[Symbol.iterator]();
 	let val = arrivals.next().value;
-	// console.log(val);
 	for (let i = 0; i < arr1.length; i++) {
 		while(val <= arr1[i]){
 			total_room -= 1;
